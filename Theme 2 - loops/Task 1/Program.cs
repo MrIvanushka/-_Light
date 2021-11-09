2f3a5fa58efa8643835d257e43619ed7c8b0f0b7
MrIvanushka <74343782+MrIvanushka@users.noreply.github.com>
2021-11-10 01:33:40 +0300

    basic loops

---
+++ Theme 2 - loops/Task 1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите сообщение: ");
+            string message = Console.ReadLine();
+            Console.Write("Введите количество повторений: ");
+            uint repeatCount = Convert.ToUInt32(Console.ReadLine());
+
+            for (uint i = 0; i < repeatCount; i++)
+                Console.WriteLine(message);
+        }
+    }
+}
