0a9dcab796f107bcedddaf57c139fd7d776fae5c
MrIvanushka <74343782+MrIvanushka@users.noreply.github.com>
2021-11-10 01:05:53 +0300

    Task5: Shop

---
+++ Task5/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int gemCurrency = 3;
+            Console.Write("Cколько у вас золота? ");
+            int goldCount = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Cколько кристаллов хотите купить? ");
+            int gemCount = Convert.ToInt32(Console.ReadLine());
+            goldCount -= gemCount * gemCurrency;
+            Console.WriteLine($"Кристаллов получено: {gemCount}");
+            Console.WriteLine($"Золота осталось: {goldCount}");
+            Console.Read();
+        }
+    }
+}
