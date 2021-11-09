4f75b811dd345a0004dedb5006fd0f7bb86508c4
MrIvanushka <74343782+MrIvanushka@users.noreply.github.com>
2021-11-10 00:50:04 +0300

    Task 3: Pictures

---
+++ Task3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            uint picturesInAlbum = 52;
+            uint setCapacity = 3;
+            uint setCount = picturesInAlbum / setCapacity;
+            uint freePicturesCount = picturesInAlbum % setCapacity;
+            Console.WriteLine($"Количество рядов: {setCount}");
+            Console.WriteLine($"Картинок сверх меры: {freePicturesCount}");
+        }
+    }
+}
