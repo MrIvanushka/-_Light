6fe0c7d31a6f74fb63332db0b2c4365d0fd655f2
MrIvanushka <74343782+MrIvanushka@users.noreply.github.com>
2021-11-10 01:28:07 +0300

    Uploading forgotten program of task 6

---
+++ Task6/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            uint singleDuration = 10;
+            Console.Write("Введите кол-во старушек: ");
+            uint ladyCount = Convert.ToUInt32(Console.ReadLine());
+
+            uint waitingTimeInHours = (ladyCount * singleDuration) / 60;
+            uint residuaryTime = (ladyCount * singleDuration) % 60;
+
+            Console.WriteLine($"Вы должны отстоять в очереди {waitingTimeInHours} часа и {residuaryTime} минут.");
+        }
+    }
+}
