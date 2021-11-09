a91b94a9b215ac28e260006d47938207f9cf8b46
MrIvanushka <74343782+MrIvanushka@users.noreply.github.com>
2021-11-09 21:31:11 +0300

    Task1: Variables

---
+++ Task1/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float hp;
+            string name;
+            bool isAlive;
+            bool isVisible;
+            int enemyCount;
+            uint money;
+            float duration;
+            float damage;
+            float armor;
+            float attackRange;
+        }
+    }
+}
