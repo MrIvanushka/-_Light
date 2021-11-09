2f01b060d5354d80bcc8eef8cef63f717df4049f
MrIvanushka <74343782+MrIvanushka@users.noreply.github.com>
2021-11-10 00:26:17 +0300

    Task 2: Dialogue

---
+++ Task2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Как вас зовут? \n");
+            string name = Console.ReadLine();
+            Console.Write("Сколько вам лет? \n");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Какой ваш знак зодиака? \n");
+            string zodiacSign = Console.ReadLine();
+            Console.Write("Кем вы работаете? \n");
+            string job = Console.ReadLine();
+            Console.Write($" зовут {name}, вам {age} лет, вы {zodiacSign} и работаете {job}.");
+        }
+    }
+}
