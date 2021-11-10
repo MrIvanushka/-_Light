c94e29aa6dfd004e649a890589c8a3a52789b109
MrIvanushka <74343782+MrIvanushka@users.noreply.github.com>
2021-11-10 12:29:46 +0300

    Task5 - arithmetic progression

---
+++ Theme 2 - loops/Task 5/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            for (uint i = 7; i < 100; i += 7)
+                Console.Write(i + " ");
+        }
+        
+    }
+}
