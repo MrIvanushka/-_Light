72a3b817da585963b36e5bef1bf41e6d2a27f54b
MrIvanushka <74343782+MrIvanushka@users.noreply.github.com>
2021-11-10 12:49:15 +0300

    Task 7 - password

---
+++ Theme 2 - loops/Task 7/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string message = "!!Тайное послание!!";
+            string rightPassword = "abcd";
+            Console.Write("Введите пароль: ");
+            string password = Console.ReadLine();
+            while(password != rightPassword)
+            {
+                Console.Write("Ошибка: пароль неверный. \nВведите пароль: ");
+                password = Console.ReadLine();
+            }
+            Console.WriteLine("!!Тайное послание!!");
+        }
+        
+    }
+}
