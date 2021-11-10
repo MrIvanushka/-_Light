5bf07e282475a16f8a137548b754dd5344dbee9c
MrIvanushka <74343782+MrIvanushka@users.noreply.github.com>
2021-11-10 12:42:46 +0300

    Task 6 - frame

---
+++ Theme 2 - loops/Task6/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            char frameSymbol = Convert.ToChar(Console.ReadLine());
+            int frameWidth = name.Length + 4;
+
+            for (int i = 0; i < frameWidth; i++)
+                Console.Write(frameSymbol);
+            Console.WriteLine($"\n{frameSymbol} {name} {frameSymbol}");
+            for (int i = 0; i < frameWidth; i++)
+                Console.Write(frameSymbol);
+            Console.Write('\n');
+        }
+        
+    }
+}
