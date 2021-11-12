85389bf08ed814e16ddc20d0a501b673fb5a708d
MrIvanushka <74343782+MrIvanushka@users.noreply.github.com>
2021-11-12 13:39:11 +0300

    task 3 - parser

---
+++ Theme 4 - functions/Task3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetInt());
+        }
+
+        static int GetInt()
+        {
+            string readValue = "";
+            int exportValue;
+
+            do
+            {
+                readValue = Console.ReadLine();
+            }
+            while (int.TryParse(readValue, out exportValue) == false);
+
+            return exportValue;
+        }
+    }
+}
