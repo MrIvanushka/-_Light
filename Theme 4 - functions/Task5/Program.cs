71dd6e7477cc776e6e37702aa9594ca18eeeb956
MrIvanushka <74343782+MrIvanushka@users.noreply.github.com>
2021-11-12 20:40:53 +0300

    Task 5 - shuffle

---
+++ Theme 4 - functions/Task5/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[20];
+            Random random = new Random();
+            Console.WriteLine("Исходный массив: ");
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = random.Next(1000);
+                Console.Write(array[i] + " ");
+            }
+
+            Shuffle(array);
+
+            Console.WriteLine("\nПеремешанный массив: ");
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+        }
+
+        static void Shuffle(int[] array)
+        {
+            Random random = new Random();
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                int randomIndex = 0;
+
+                do
+                    randomIndex = random.Next(array.Length);
+                while (randomIndex == i);
+
+                Swap(ref array[i], ref array[randomIndex]);
+            }   
+        }
+
+        static void Swap(ref int a, ref int b)
+        {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+    }
+}
