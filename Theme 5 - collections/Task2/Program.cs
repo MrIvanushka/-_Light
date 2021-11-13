525d7639001baf4d9894fd6545c097cc3b4201e8
MrIvanushka <74343782+MrIvanushka@users.noreply.github.com>
2021-11-13 13:48:51 +0300

    task2 - shop

---
+++ Theme 5 - collections/Task2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> purchasePool = new Queue<int>();
+            int purchaseCount = 5;
+            Random random = new Random();
+
+            for (int i = 0; i < purchaseCount; i++)
+                purchasePool.Enqueue(random.Next(20));
+
+            int accountMoney = 0;
+
+            while(purchasePool.Count > 0)
+            {
+                accountMoney += purchasePool.Peek();
+                Console.WriteLine($"Покупка {purchasePool.Dequeue()}$, на счёте {accountMoney}$");
+                Console.ReadKey();
+                Console.Clear();
+            }
+            Console.WriteLine($"Покупки окончены. На счёте {accountMoney}$");
+        }
+    }
+}
