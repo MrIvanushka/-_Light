5b5610cd58981001fa01fef47274b8dea1aa8b1e
MrIvanushka <74343782+MrIvanushka@users.noreply.github.com>
2021-11-13 14:07:27 +0300

    task3 - dynamic array

---
+++ Theme 5 - collections/Task3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>();
+            string command = "";
+
+            do
+            {
+                command = Console.ReadLine();
+
+                try
+                {
+                    int newElement = Convert.ToInt32(command);
+                    list.Add(newElement);
+                }
+                catch (FormatException)
+                {
+                    if (command == "sum")
+                    {
+                        int sum = 0;
+                        list.ForEach(element => sum += element);
+                        Console.WriteLine(sum);
+                    }
+                    else if (command != "exit")
+                    {
+                        Console.WriteLine("Неизвестная команда.");
+                    }
+                }
+            }
+            while (command != "exit");
+        }
+    }
+}
