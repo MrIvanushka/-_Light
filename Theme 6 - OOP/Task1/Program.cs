762293a8bca6499326daa9642987941b4e1bd018
MrIvanushka <74343782+MrIvanushka@users.noreply.github.com>
2021-11-14 21:15:52 +0300

    First OOP tasks

---
+++ Theme 6 - OOP/Task1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Player player1 = new Player();
+            Player player2 = new Player("Steve");
+            Console.WriteLine("Player1: " + player1);
+            Console.WriteLine("Player2: " + player2);
+        }
+    }
+
+
+    class Player
+    {
+        private string _name;
+        private float _hp;
+        private float _maxHP = 100;
+
+        public Player() : this("Username")
+        { }
+
+        public Player(string name)
+        {
+            _name = name;
+            _hp = _maxHP;
+        }
+
+        public override string ToString()
+        {
+            return $"[{_name}: {_hp} HP.]";
+        }
+    }
+
+
+}
---
+++ Theme 6 - OOP/Task2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Renderer renderer = new Renderer();
+            Player player1 = new Player();
+            Player player2 = new Player(3, 3, '#');
+            renderer.Draw(player1);
+            renderer.Draw(player2);
+            Console.Read();
+        }
+    }
+
+    class Renderer
+    {
+        public void Draw(Player player)
+        {
+            Console.SetCursorPosition(player.X, player.Y);
+            Console.Write(player.Icon);
+        }
+    }
+
+    class Player
+    {
+        public int X { get; private set; }
+        public int Y { get; private set; }
+        public char Icon { get; private set; }
+
+        public Player() : this(0, 0)
+        { }
+
+        public Player(int x, int y) : this(x, y, '@')
+        { }
+
+        public Player(int x, int y, char icon)
+        {
+            X = x;
+            Y = y;
+            Icon = icon;
+        }        
+    }
+
+
+}
