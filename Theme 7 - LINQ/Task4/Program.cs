a79b805f778309c975aaa71e70a48a0ea4aaf029
MrIvanushka <74343782+MrIvanushka@users.noreply.github.com>
2021-11-24 20:50:04 +0300

    Another LINQ tasks

---
+++ Theme 7 - LINQ/Task4/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            User[] allUsers = new User[] {
+                new User("Джон", 15, 100),
+                new User("Иван", 30, 80),
+                new User("Борис", 43, 120),
+                new User("Армен", 28, 20),
+                new User("Андрей", 15, 48),
+                new User("Дима", 31, 34),
+                new User("Катя", 27, 58),
+                new User("Ислам", 40, 34),
+                new User("Артём", 12, 2),
+                new User("Денис", 34, 43),
+                new User("Даша", 33, 43),
+                new User("Сергей", 68, 76)
+            };
+            int topUsersCount = 3;
+
+            User[] levelLeaders = allUsers.OrderByDescending(user => user.Level).Take(topUsersCount).ToArray();
+            User[] powerLeaders = allUsers.OrderByDescending(user => user.Power).Take(topUsersCount).ToArray();
+
+            Console.WriteLine($"Топ {topUsersCount} игроков по уровню:");
+
+            for(int i = 0; i < levelLeaders.Length; i++)
+                Console.WriteLine(i+1 + ". " + levelLeaders[i].Name);
+
+            Console.WriteLine($"Топ {topUsersCount} игроков по силе:");
+
+            for (int i = 0; i < powerLeaders.Length; i++)
+                Console.WriteLine(i+1 + ". " + powerLeaders[i].Name);
+        }
+    }
+
+    class User
+    {
+        public string Name { get; private set; }
+        public int Level { get; private set; }
+        public int Power { get; private set; }
+
+        public User(string name, int level, int power)
+        {
+            Name = name;
+            Level = level;
+            Power = power;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} Уровень {Level} Сила {Power}";
+        }
+    }
+    
+
+    
+}
---
+++ Theme 7 - LINQ/Task5/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            CannedMeat[] allCans = new CannedMeat[] {
+                new CannedMeat("Барс", 2019, 3),
+                new CannedMeat("Совок", 2015, 3),
+                new CannedMeat("Гродфуд", 2018, 6),
+                new CannedMeat("Русский изыск", 2016, 2),
+                new CannedMeat("Агрокомбинат СНОВ", 2019, 2),
+                new CannedMeat("ОВА", 2017, 5)
+            };
+            int currentYear = 2021;
+            var expiredCans = allCans.Where(can => can.IsExpired(currentYear) == true);
+
+            if (expiredCans.Count() > 0)
+            {
+                Console.WriteLine("Просроченные банки: ");
+
+                foreach (var can in expiredCans)
+                    Console.WriteLine(can.Name);
+            }
+            else
+            {
+                Console.WriteLine("Просрочки нет.");
+            }
+        }
+    }
+
+    class CannedMeat
+    {
+        public string Name { get; private set; }
+        private int _yearOfManufacture;
+        private int _storageDuration;
+
+        public CannedMeat(string name, int yearOfManufacture, int storageDuration)
+        {
+            Name = name;
+            _yearOfManufacture = yearOfManufacture;
+            _storageDuration = storageDuration;
+        }
+
+        public bool IsExpired(int currentYear)
+        {
+            return _yearOfManufacture + _storageDuration < currentYear;
+        }
+    }
+    
+
+    
+}
